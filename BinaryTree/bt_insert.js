a873c578aa7c67fbd5ec23a67844a175f0b4cae4
sergiozuloaga10 <sergio-sst@hotmail.com>
2023-11-15 11:02:50 -0600

    Ejercicio busqueda

---
+++ BinaryTree/bt_insert.js
@@ -0,0 +1,31 @@
+class Node {
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// Tarea de insertar
+function bt_insert(root, value) {
+    if (root === null) {
+        return new Node(value);
+    }
+
+    if (root.value > value) {
+        root.left = bt_insert(root.left, value);
+    } else {
+        root.right = bt_insert(root.right, value);
+    }
+
+    return root;
+}
+
+// Insertamos
+let root = new Node(10);
+root = bt_insert(root, 5);
+root = bt_insert(root, 15);
+root = bt_insert(root, 3);
+root = bt_insert(root, 7);
+
+console.log(root);
---
+++ BinaryTree/bt_search.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// Tarea de insertar
+function bt_insert(root, value) {
+    if (root === null) {
+        return new Node(value);
+    }
+
+    if (root.value > value) {
+        root.left = bt_insert(root.left, value);
+    } else {
+        root.right = bt_insert(root.right, value);
+    }
+
+    return root;
+}
+
+// Lo vimos en clase: busqueda
+function search(root, t) {
+    if (root === null || root.value === t) {
+        return root;
+    }
+
+    if (root.value > t) {
+        return search(root.left, t);
+    } else {
+        return search(root.right, t);
+    }
+}
+
+// Insertamos
+let root = new Node(10);
+root = bt_insert(root, 5);
+root = bt_insert(root, 15);
+root = bt_insert(root, 3);
+root = bt_insert(root, 7);
+
+console.log(search(root, 5)); // Devuelve el nodo con valor 15
+
